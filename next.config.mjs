4a84b68f875800a1a2db886bf9a1b5555ddd846d
MatosLarissa <larissamatos88@outlook.com>
2024-04-10 19:35:11 -0300

    🎉 chore: initial setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    eslint: {
+    ignoreDuringBuilds: true,
+    },
+    env: {
+        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+        NEXT_PUBLIC_CART_STORAGE_KEY: process.env.NEXT_PUBLIC_CART_STORAGE_KEY
+    }
+};
 
 export default nextConfig;
