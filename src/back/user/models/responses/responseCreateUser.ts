90e2c07cec2435498a105735ea05d8a1192ef0b2
MatosLarissa <larissamatos88@outlook.com>
2024-04-14 22:13:32 -0300

    feat: Implement response classes for user-related operations

---
+++ src/back/user/models/responses/responseCreateUser.ts
@@ -0,0 +1,39 @@
+import LinkInterface from "../../../utils/responses/link.interface"
+import IUser from "../../interfaces/user.interface"
+
+export default class ResponseCreateUser implements IUser {
+  readonly id: string;
+  readonly name: string;
+  readonly birthDate: Date;
+  readonly email: string;
+  readonly password: string;
+  readonly createdAt: string;
+  readonly updatedAt: string;
+  readonly lastLogin: string;
+  readonly token: string;
+  readonly user: IUser;
+  readonly links: LinkInterface[];
+
+  constructor(user: IUser, token: string, link: LinkInterface[]) {
+    this.user = {
+      id: user.id,
+      name: user.name,
+      birthDate: user.birthDate,
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      lastLogin: user.lastLogin,
+    }
+    this.token = token
+    this.links = link
+  }
+
+  toResponse() {
+    return {
+      user: this.user,
+      token: this.links,
+      links: this.links,
+    }
+  }
+}
---
+++ src/back/user/models/responses/responseUser.ts
@@ -0,0 +1,36 @@
+import LinkInterface from "../../../utils/responses/link.interface"
+import IUser from "../../interfaces/user.interface"
+
+export default class ResponseUser implements IUser {
+  readonly id: string;
+  readonly name: string;
+  readonly birthDate: Date;
+  readonly email: string;
+  readonly password: string;
+  readonly createdAt: string;
+  readonly updatedAt: string;
+  readonly lastLogin: string;
+  readonly user: IUser;
+  readonly links: LinkInterface[];
+
+  constructor(user: IUser, link: LinkInterface[]) {
+    this.user = {
+      id: user.id,
+      name: user.name,
+      birthDate: user.birthDate,
+      email: user.email,
+      password: user.password,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      lastLogin: user.lastLogin,
+    }
+    this.links = link
+  }
+
+  toResponse() {
+    return {
+      user: this.user,
+      links: this.links,
+    }
+  }
+}
