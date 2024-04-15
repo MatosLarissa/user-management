14b20c472792807f85f583dfa79eba15f5e76f1b
MatosLarissa <larissamatos88@outlook.com>
2024-04-14 23:01:55 -0300

    feat: Implement Header component

---
+++ src/front/component/header/header.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import style from "./header.module.sass"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useUserContext } from "@/front/context/useUserContext"
+
+export default function Header() {
+  const pathname = usePathname()
+  const router = useRouter()
+  const { token, clearToken } = useUserContext()
+
+  function logout (){
+    clearToken()
+    localStorage.removeItem("userToken")
+    router.refresh()
+    router.push("/login")
+  }
+
+  return (
+    <header className={style.header}>
+      <div className={style.containerLogo}>
+        <Link href="/" className={style.logo}>User Management</Link>
+      </div>
+      <nav>
+        <ul className={style.navList}>
+          <li className={style.navItem}>
+            <Link href="/"  className={pathname === "/" ? style.linkNoHover : style.link}>
+                            Home
+            </Link>
+          </li>
+          {token ? (
+            <li className={style.navItem}>
+              <div onClick={logout} className={style.link}>
+                  logout
+              </div>
+            </li>
+          ) : (
+            ""
+          )}
+          <li className={style.navItem}>
+            <Link href="/about" className={pathname === "/about" ? style.linkNoHover : style.link}>
+                            About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
